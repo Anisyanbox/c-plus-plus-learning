4221fc8ad94e33123c44e81b08b68e166bdbbf80
anisyan <anisimov.alexander.s@gmail.com>
2019-04-28 20:04:06 +0300

    added main program skeleton

--- triangular-numbers/main.cpp
+++ triangular-numbers/main.cpp
@@ -1,5 +1,48 @@
 #include <iostream>
+#include <string>
+#include <cctype>
 
+static int kWrongInputSequence = 0;
+
+// -----------------------------------------------------------------------------
+static bool IsInputArgTrue(std::string s) {
+  if (s.size() == 0) {
+  return false;
+  }
+  for(std::string::iterator it = s.begin(); it < s.end(); it++) {
+    if (it == s.begin() && *it == '+') {
+      continue;
+    }
+    if (!std::isdigit((int)*it)) {
+      return false;
+    }
+  }
+  return true;
+}
+
+// -----------------------------------------------------------------------------
+static unsigned long long GetNumberFromString(std::string s) {
+  std::string::size_type sz = 0;
+  return std::stoull(static_cast<const std::string&>(s), &sz, 10);
+}
+
+// -----------------------------------------------------------------------------
+static unsigned long long GetTriangularIndexIfPossible(unsigned long long n) {
+  if (n == 0) {
+    return 0;
+  }
+  return 0;
+}
+
+// -----------------------------------------------------------------------------
 int main (int argc, char * argv[]) {
-    return EXIT_SUCCESS;
+  std::string input;
+
+  std::getline(std::cin, input);      
+  if (!IsInputArgTrue(input)) {
+      std::cout << kWrongInputSequence;
+  } else {
+      std::cout << GetTriangularIndexIfPossible(GetNumberFromString(input));
+  }
+  return EXIT_SUCCESS;
 }
